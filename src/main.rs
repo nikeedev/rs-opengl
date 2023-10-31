2c578903c2a99a87949d5154ec2dac39453258cb
nikeedev <nikita.gon.dev@gmail.com>
2023-10-31 12:10:54 +0100

    up

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use gl::types::*;
 use glfw::{Action, Context, Key};
+use std::ffi::CString;
 use std::fmt;
+use std::mem;
+use std::mem::size_of;
+use std::ptr;
+use std::str;
 
 #[derive(Debug)]
 struct Vec2 {
@@ -39,12 +44,103 @@ impl fmt::Display for Vec2 {
     }
 }
 
+fn compile_shader(src: &str, ty: GLenum) -> GLuint {
+    let shader;
+    unsafe {
+        shader = gl::CreateShader(ty);
+        // Attempt to compile the shader
+        let c_str = CString::new(src.as_bytes()).unwrap();
+        gl::ShaderSource(shader, 1, &c_str.as_ptr(), ptr::null());
+        gl::CompileShader(shader);
+
+        // Get the compile status
+        let mut status = gl::FALSE as GLint;
+        gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut status);
+
+        // Fail on error
+        if status != (gl::TRUE as GLint) {
+            let mut len = 0;
+            gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf = vec![0; len as usize]; // subtract 1 to skip the trailing null character
+            gl::GetShaderInfoLog(
+                shader,
+                len,
+                ptr::null_mut(),
+                buf.as_mut_ptr() as *mut GLchar,
+            );
+            panic!(
+                "{}",
+                str::from_utf8(&buf)
+                    .ok()
+                    .expect("ShaderInfoLog not valid utf8")
+            );
+        }
+    }
+
+    shader
+}
+
+fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
+    unsafe {
+        let program = gl::CreateProgram();
+        gl::AttachShader(program, vs);
+        gl::AttachShader(program, fs);
+        gl::DeleteShader(vs);
+        gl::DeleteShader(fs);
+        gl::LinkProgram(program);
+        // Get the link status
+        let mut status = gl::FALSE as GLint;
+        gl::GetProgramiv(program, gl::LINK_STATUS, &mut status);
+
+        // Fail on error
+        if status != (gl::TRUE as GLint) {
+            let mut len: GLint = 0;
+            gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf = vec![0; len as usize];
+            // subtract 1 to skip the trailing null character
+            gl::GetProgramInfoLog(
+                program,
+                len,
+                ptr::null_mut(),
+                buf.as_mut_ptr() as *mut GLchar,
+            );
+            panic!(
+                "{}",
+                str::from_utf8(&buf)
+                    .ok()
+                    .expect("ProgramInfoLog not valid utf8")
+            );
+        }
+        program
+    }
+}
+
+static VS_SRC: &'static str = "
+#version 330 core
+layout (location = 0) in vec3 aPos;
+out vec4 color;
+
+void main() {
+    gl_Position = vec4(aPos, 1.0);
+    color = vec4(0.2, 0.4, 0.8, 1.0);
+}";
+
+static FS_SRC: &'static str = "
+#version 330 core
+in vec4 color;
+out vec4 FragColor;
+
+void main() {
+    FragColor = color;
+}";
+
+static VERTEX_DATA: [GLfloat; 9] = [-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0];
+
 fn main() {
     let mut screensize = Vec2::new(800, 600);
     use glfw::fail_on_errors;
     let mut glfw = glfw::init(fail_on_errors!()).unwrap();
 
-    // Create a windowed mode window and its OpenGL context
     let (mut window, events) = glfw
         .create_window(
             screensize.x,
@@ -54,11 +150,59 @@ fn main() {
         )
         .expect("Failed to create GLFW window.");
 
+    // the supplied function must be of the type:
+    // `&fn(symbol: &'static str) -> *const std::os::raw::c_void`
+    gl::load_with(|s| window.get_proc_address(s));
+
+    // loading a specific function pointer
+    gl::Viewport::load_with(|s| window.get_proc_address(s));
+
+    // Create a windowed mode window and its OpenGL context
     // Make the window's context current
     window.make_current();
     window.set_key_polling(true);
 
-    window.set_framebuffer_size_callback(framebuffer_size_callback);
+    // Create GLSL shaders
+    let vs = compile_shader(VS_SRC, gl::VERTEX_SHADER);
+    let fs = compile_shader(FS_SRC, gl::FRAGMENT_SHADER);
+    let program = link_program(vs, fs);
+
+    // Triangle - Vertex
+    let mut vao = 0;
+    let mut vbo = 0;
+
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+
+        gl::GenBuffers(1, &mut vbo);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (VERTEX_DATA.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            mem::transmute(&VERTEX_DATA[0]),
+            gl::STATIC_DRAW,
+        );
+
+        // Use shader program
+        gl::UseProgram(program);
+        gl::BindFragDataLocation(program, 0, CString::new("color").unwrap().as_ptr());
+
+        // Specify the layout of the vertex data
+
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE as GLboolean,
+            3 * mem::size_of::<f32>() as i32,
+            ptr::null(),
+        );
+
+        gl::EnableVertexAttribArray(0);
+
+        gl::BindVertexArray(0);
+    };
 
     let mut temp_screensize = screensize;
     // Loop until the user closes the window
@@ -73,6 +217,15 @@ fn main() {
         unsafe {
             gl::ClearColor(0.39, 0.58, 0.92, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            // Rendering stuff here:
+
+            gl::UseProgram(program);
+            gl::BindVertexArray(vao);
+
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+
+            gl::BindVertexArray(0);
         };
 
         screensize = Vec2::from(glfw::Window::get_size(&window));
@@ -85,10 +238,13 @@ fn main() {
         window.swap_buffers();
         glfw.poll_events();
     }
-}
 
-fn framebuffer_size_callback(width: i32, height: i32) {
-    unsafe { gl::Viewport(0, 0, width, height) };
+    unsafe {
+        gl::DeleteVertexArrays(1, &vao);
+        gl::DeleteBuffers(1, &vbo);
+        gl::DeleteProgram(program);
+    };
+
 }
 
 fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
